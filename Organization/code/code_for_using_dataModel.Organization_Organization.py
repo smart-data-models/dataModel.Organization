ee1d37d224a316b53bdb745a7fc895e623a3f9b5
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:42:22 +0200

    created example of code

--- Organization/code/code_for_using_dataModel.Organization_Organization.py
+++ Organization/code/code_for_using_dataModel.Organization_Organization.py
@@ -24,25 +24,30 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Organization"
 subject = "dataModel.Organization"
-url = "{'type': 'Property', 'value': 'https://www.example-organization-homepage.com'}"
+url = "https://www.example-organization-homepage.com"
 attribute = "url"
 value = url
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-legalName = "{'type': 'Property', 'value': 'Beispielname GmbH'}"
+legalName = "Beispielname GmbH"
 attribute = "legalName"
 value = legalName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-taxID = "{'type': 'Property', 'value': '12345678900'}"
+taxID = "12345678900"
 attribute = "taxID"
 value = taxID
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
